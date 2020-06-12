267e8f1df7cfa775192a3574daff363d83669f07
Vladimir Smetankin <vs.smetankin@gmail.com>
2020-06-12 12:50:31 +0300

    add buttons hover and new mobile menu design

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,9 +1,22 @@
 (function () {
     var toggle = document.querySelector('.nav-toggle');
     var menu = document.getElementById('mobile-menu');
+    var close = document.getElementsByClassName('close-link');
 
     toggle.addEventListener('click', function(e) {
         this.classList.toggle('opened');
         menu.classList.toggle('mobile-close-menu');
+        console.log(close);
     });
+    // close.map((item)=> {
+    //     item.addEventListener('click', function (e) {
+    //         menu.classList.toggle('mobile-close-menu')
+    //     })
+    // })();
+    for(let i = 0; i<close.length; i++){
+        close[i].addEventListener('click', function (e) {
+            menu.classList.toggle('mobile-close-menu')
+            toggle.classList.toggle('opened');
+        })
+    }
 })();
